65b895f5a34ee7a3cfd062f1a5b1829ea4ccdff3
Isaac Patrocinio <isaacpatrocinio27@gmail.com>
2026-01-14 18:03:01 -0300

    Implemented position class

---
+++ ChessConsole/Programa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tabuleiro;
+
+namespace ChessConsole
+{
+    public class Programa
+    {
+        static void Main(string[] args)
+        {
+            Posicao posicao = new(2,6);
+            Console.WriteLine(posicao);
+        }
+    }
+}
---
+++ ChessConsole/Tabuleiro/Posicao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuleiro
+{
+    public struct Posicao
+    {
+        public int Linha;
+        public int Coluna;
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return $"{Linha}, {Coluna}";
+        }
+    }
+}
